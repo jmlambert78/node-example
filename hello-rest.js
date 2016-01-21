bec3a2e483b0042eb5e81b64d699ca2fc5380ebe
jmlambert78 <dummy@gmail.com>
2016-01-21 09:44:46 +0000

    Updated page hello-rest.js

--- hello-rest.js
+++ hello-rest.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from the fabric8 NodeJS example');
+  res.end('Hello JML from the fabric8 NodeJS example');
 }).listen(8080);
 console.log('JML Server running at http://localhost:8080/');
