8079fbb4fd156a6f6b010fc336703a9da6acc330
jmlambert78 <jmlambert78@gmail.com>
2016-01-22 18:48:29 +0100

    Update hello-rest.js

--- hello-rest.js
+++ hello-rest.js
@@ -2,7 +2,7 @@ var http = require('http');
 for (var item in process.env) 
 console.log(item," = ",process.env[item]);
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("<H1>Node Envvars</H1>");
   res.write("<table>");
 for (var item in process.env) 
