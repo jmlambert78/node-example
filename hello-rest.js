b544f747fd82dc927608f28929b79bfa947b229c
jmlambert78 <dummy@gmail.com>
2016-02-11 19:21:15 +0000

    Updated page hello-rest.js

--- hello-rest.js
+++ hello-rest.js
@@ -6,6 +6,7 @@ http.createServer(function (req, res) {
   res.write("<H1>Node Envvars</H1>");
   res.write("<table>");
 for (var item in process.env) 
+  if ((item.substring(0, 3) === "npm")==false)
   res.write("<TR><TD>"+item+"</TD><TD>"+process.env[item]+"</TD></TR>");
   res.write("</table>");
   res.end();
